783293e5027f712702e2ccdf9fd9bcc431a8b192
Makar <dezhemesov.work@gmail.com>
2026-01-23 21:48:02 +0100

    add basic settings

---
+++ LiveOpsClient/Assets/Scripts/Editor/BootSceneLoader.cs
@@ -0,0 +1,60 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public static class BootSceneLoader
+    {
+        private const string BootScenePath = "Assets/Scenes/Boot.unity";
+        private const string EnabledPrefKey = "BootSceneLoader_Enabled";
+
+        public static bool IsEnabled
+        {
+            get => EditorPrefs.GetBool(EnabledPrefKey, true);
+            set
+            {
+                EditorPrefs.SetBool(EnabledPrefKey, value);
+                UpdatePlayModeStartScene();
+            }
+        }
+
+        static BootSceneLoader()
+        {
+            EditorApplication.delayCall += UpdatePlayModeStartScene;
+        }
+
+        private static void UpdatePlayModeStartScene()
+        {
+            if (!IsEnabled)
+            {
+                EditorSceneManager.playModeStartScene = null;
+                return;
+            }
+
+            var bootScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(BootScenePath);
+            EditorSceneManager.playModeStartScene = bootScene;
+        }
+
+        [MenuItem("Tools/Boot Scene Loader/Enable", true)]
+        private static bool EnableValidate() => !IsEnabled;
+
+        [MenuItem("Tools/Boot Scene Loader/Enable")]
+        private static void Enable() => IsEnabled = true;
+
+        [MenuItem("Tools/Boot Scene Loader/Disable", true)]
+        private static bool DisableValidate() => IsEnabled;
+
+        [MenuItem("Tools/Boot Scene Loader/Disable")]
+        private static void Disable() => IsEnabled = false;
+
+        [MenuItem("Tools/Boot Scene Loader/Ping Boot Scene")]
+        private static void PingBootScene()
+        {
+            var bootScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(BootScenePath);
+            EditorGUIUtility.PingObject(bootScene);
+            Selection.activeObject = bootScene;
+        }
+    }
+}
