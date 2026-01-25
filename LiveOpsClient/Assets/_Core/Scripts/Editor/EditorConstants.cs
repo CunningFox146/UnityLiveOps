d4ebceba36a97296c99aa5191da9bcc1b2c41131
Makar <dezhemesov.work@gmail.com>
2026-01-25 16:02:52 +0100

    Fix naming

--- LiveOpsClient/Assets/_Core/Scripts/Editor/EditorConstants.cs
+++ LiveOpsClient/Assets/_Core/Scripts/Editor/EditorConstants.cs
@@ -2,7 +2,7 @@ namespace Editor
 {
     public class EditorConstants
     {
-        public const string BootScenePath = "Assets/Assets/Scenes/Boot.unity";
+        public const string BootScenePath = "Assets/_Core/Scenes/Boot.unity";
         public const string ToolsPath = "Tools/";
         public const string AddressablesPath = ToolsPath + "Addressables/";
         public const string BootSceneToolsPath = ToolsPath + "Boot Scene/";
